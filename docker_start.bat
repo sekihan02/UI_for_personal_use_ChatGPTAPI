58c834b47a4101eec59385732c6646b65a8e338e
sekihan02 <sekihan0291@gmail.com>
2023-06-26 04:32:39 +0900

    LICENSEの追加

--- docker_start.bat
+++ docker_start.bat
@@ -1,3 +1,2 @@
 @REM 2回目以降の起動時
-@REM docker run -p 5000:5000 --rm -v C:\Users\Public\Documents\github\Docker_env\chat_gpt\work:/work -w /work --name chat_gpt_env chat_env
 docker run -p 5000:5000 --rm --name chat_gpt_env chat_env
